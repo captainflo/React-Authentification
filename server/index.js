faab2b1dabd4210ef55ef012fef641ec3f3c8b10
florian lahitte <lahitte.florian@gmail.com>
2019-07-01 10:42:57 -0400

    add server setup and middleware

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App Setup
+app.use(morgan('combined')); /* login server in your terminal */
+app.use(bodyParser.json({type: '*/*'})); /* used to parse incoming requests */
+
+
+// Server Setup
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
