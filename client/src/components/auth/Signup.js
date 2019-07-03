7820641b4f36d8f4d2471ac2195255c363ad2648
florian lahitte <lahitte.florian@gmail.com>
2019-07-03 13:22:01 -0400

    signup form react and react-router-dom

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Signup extends React.Component{
+    render(){
+        return(
+            <div>
+                <form>
+                    <fieldset>
+                        <label>Email</label>
+                        
+                    </fieldset>
+                    <fieldset>
+                        <label>Password</label>
+                    </fieldset>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Signup;
