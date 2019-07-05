08bfe4de0ae37ae238e4253475ddaa22fdca0c65
florian lahitte <lahitte.florian@gmail.com>
2019-07-05 11:25:31 -0400

    final version MERN

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,9 +4,30 @@ import { AUTH_USER, AUTH_ERROR } from './types';
 export const signup = (formProps, callback) => async dispatch =>{
     try {
     const response = await axios.post('http://localhost:3001/signup', formProps);
-    dispatch({type: AUTH_USER, payload: response.data.token})
+    dispatch({type: AUTH_USER, payload: response.data.token});
+    localStorage.setItem('token', response.data.token);
     callback(); /* history callback*/
     } catch (e){
         dispatch({type: AUTH_ERROR, payload: 'Email in use'});
+    }
+};
+
+export const signout = ()=>{
+    localStorage.removeItem('token');
+
+    return {
+        type: AUTH_USER,
+        payload:''
+    }
+}
+
+export const signin = (formProps, callback) => async dispatch =>{
+    try {
+    const response = await axios.post('http://localhost:3001/signin', formProps);
+    dispatch({type: AUTH_USER, payload: response.data.token});
+    localStorage.setItem('token', response.data.token);
+    callback(); /* history callback*/
+    } catch (e){
+        dispatch({type: AUTH_ERROR, payload: 'Invalid login credentials'});
     }
 };
